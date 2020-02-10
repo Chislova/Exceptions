3fd4fc08e8ab36151416836985383f3f1d81dbc0
Chislova <44863427+Chislova@users.noreply.github.com>
2020-02-10 15:14:37 +0300

    Delete emptyDishwasher.java

--- emptyDishwasher.java
+++
@@ -1,9 +0,0 @@
-
-public class emptyDishwasher extends Exception {
-
-    public emptyDishwasher() {
-        super("Can't start; add some dishes first!");
-        System.out.println("For further assistance read the instruction.");
-
-    }
-}
