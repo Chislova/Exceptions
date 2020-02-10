9673a4aded8c4a4834998e57f3560cc735440e6f
Chislova <44863427+Chislova@users.noreply.github.com>
2020-02-10 15:14:58 +0300

    Delete programIsRunning.java

--- programIsRunning.java
+++
@@ -1,6 +0,0 @@
-public class programIsRunning extends Exception {
-    public programIsRunning() {
-        super("Can't add dish during cycle! For further assistance read the instruction.");
-
-    }
-}
